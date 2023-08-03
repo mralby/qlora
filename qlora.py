7473273891d0014956ddaed52d06b2abe3e30258
Alberto Mordonini <albmord@gmail.com>
2023-08-03 17:18:09 +0200

    fixed falcon tokenizer issue

--- qlora.py
+++ qlora.py
@@ -354,12 +354,16 @@ def get_accelerate_model(args, checkpoint_dir):
         trust_remote_code=args.trust_remote_code,
         use_auth_token=args.use_auth_token,
     )
-    if tokenizer._pad_token is None:
-        smart_tokenizer_and_embedding_resize(
-            special_tokens_dict=dict(pad_token=DEFAULT_PAD_TOKEN),
-            tokenizer=tokenizer,
-            model=model,
-        )
+    
+    if 'falcon' in args.model_name_or_path:
+        tokenizer.pad_token = tokenizer.eos_token
+    else:
+        if tokenizer._pad_token is None:
+            smart_tokenizer_and_embedding_resize(
+                special_tokens_dict=dict(pad_token=DEFAULT_PAD_TOKEN),
+                tokenizer=tokenizer,
+                model=model,
+            )
         
     if 'llama' in args.model_name_or_path or isinstance(tokenizer, LlamaTokenizer):
         # LLaMA tokenizer may not have correct special tokens set.
