17e0907bb01d23b1d884e1d5b9391f94f9a6e2dc
Alberto Mordonini <albmord@gmail.com>
2023-08-03 12:08:14 +0200

    refactor

--- qlora.py
+++ qlora.py
@@ -354,8 +354,6 @@ def get_accelerate_model(args, checkpoint_dir):
         trust_remote_code=args.trust_remote_code,
         use_auth_token=args.use_auth_token,
     )
-    if 'falcon' in args.model_name_or_path:
-        tokenizer.pad_token = tokenizer.eos_token
     if tokenizer._pad_token is None:
         smart_tokenizer_and_embedding_resize(
             special_tokens_dict=dict(pad_token=DEFAULT_PAD_TOKEN),
@@ -363,7 +361,7 @@ def get_accelerate_model(args, checkpoint_dir):
             model=model,
         )
         
-    if 'llama' in args.model_name_or_path or 'falcon' in args.model_name_or_path or isinstance(tokenizer, LlamaTokenizer):
+    if 'llama' in args.model_name_or_path or isinstance(tokenizer, LlamaTokenizer):
         # LLaMA tokenizer may not have correct special tokens set.
         # Check and add them if missing to prevent them from being parsed into different tokens.
         # Note that these are present in the vocabulary.
